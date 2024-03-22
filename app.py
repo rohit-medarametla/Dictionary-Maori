c46ad0575a96fd166c3c3d59f7d0adc65c12f3b0
20320 <20320@wc.school.nz>
2024-03-22 13:23:30 +1300

    and added style sheet code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello Worgiugild!'
 
+@app.route('/home')
+def render_contact_page():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
