b335d82f29d1d398f79dd5873617833e6662bab7
Rohit Medarametla <20320@wc.school.nz>
2024-04-14 09:59:45 +1200

    setting up 1

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello Worgiugild!'
-
-@app.route('/home')
-def render_contact_page():
-    return render_template('home.html')
+def render_homepage():
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run()
