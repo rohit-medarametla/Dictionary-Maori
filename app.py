eaf5c4a1797a5bc1e4ddc4d719a3f00971e31f97
Rohit Medarametla <20320@wc.school.nz>
2024-04-14 20:41:56 +1200

    adding home page

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Worgiugild!'
-
-@app.route('/home')
-def render_contact_page():
     return render_template('home.html')
+
+
 
 if __name__ == '__main__':
     app.run()
