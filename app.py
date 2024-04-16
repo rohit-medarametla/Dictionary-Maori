b49b15b63c785cee032214cafc3c4f38671d2074
Rohit Medarametla <20320@wc.school.nz>
2024-04-17 11:25:37 +1200

    Fixed my errors with sign up

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
 
-DATABASE = "C:/Users/Admin/OneDrive - Wellington College/Dictionary-Maori"
+DATABASE = "C:/Users/Admin/OneDrive - Wellington College/Dictionary-Maori/Maori.db"
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "qwertyuiop"
@@ -11,11 +11,11 @@ app.secret_key = "qwertyuiop"
 def create_connection(db_file):
     try:
         connection = sqlite3.connect(db_file)
+        print("Connection to database successful.")
         return connection
     except Error as e:
-        print(e)
-    return None
-
+        print("Error connecting to database:", e)
+        return None
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -27,51 +27,60 @@ def is_logged_in():
     else:
         return True
 
-def is_teacher():
-    if session.get('is_teacher') != 1:
+def if_teacher():
+    if session.get('is_teacher')  != 1:
+        print("student")
         return False
     else:
+        print("teacher")
         return True
 
-def put_data(query, params):
-    con = create_connection(DATABASE)
-    cur = con.cursor()
-    cur.execute(query, params)
-    con.commit()
-    con.close()
+
+
 
 @app.route('/signup', methods=['POST', 'GET'])
 def render_signup():
     if is_logged_in():
         return redirect('/')
     if request.method == 'POST':
-        fname = request.form.get('fname').title().strip()
+        print(request.form)
+        fname = request.form.get('fname').title()
         lname = request.form.get('lname').title().strip()
         email = request.form.get('email').lower().strip()
         year_group = request.form.get('year_group').title().strip()
-        class_name = request.form.get('class_name').title().strip()
+        class_name = request.form.get('class_name').lower().strip()
         password = request.form.get('password')
         password2 = request.form.get('password2')
         teacher = 0
         if request.form.get('is_teacher') == 'on':
             teacher = 1
 
+
         if password != password2:
-            return redirect('\signup?error=Passwords+do+not+match')
+            return redirect("\signup?error=Passwords+do+not+match")
 
         if len(password) < 8:
-            return redirect('/signup?error=Passwords+must+be+at+least+8+characters')
+            return redirect("\signup?error=Password+must+be+at+least+8+characters")
 
         hashed_password = bcrypt.generate_password_hash(password)
+        con = create_connection(DATABASE)
+        query = ("INSERT INTO user (fname, lname, email, password, is_teacher, year_group, class_name) "
+                 "VALUES (?, ?, ?, ?, ?, ?, ?)")
+        cur = con.cursor()
 
         try:
-            put_data('INSERT INTO user (fname, lname, email, password, is_teacher, year_group, class ) VALUES (?, ?, ?, ?, ?, ?, ?, )', (fname, lname, email, hashed_password, is_teacher, year_group, class_name ))
+            cur.execute(query, (fname, lname, email, hashed_password, teacher, year_group, class_name))
+            con.commit()
         except sqlite3.IntegrityError:
-            return redirect('/signup?error=Email+is+already+used')
+            con.close()
+            return redirect('\signup?error=Email+is+already+used')
 
-        return redirect('/login')
 
-    return render_template('signup.html', logged_in=is_logged_in(), is_teacher=is_teacher())
+        con.close()
+
+        return redirect("\login")
+
+    return render_template('signup.html', logged_in=is_logged_in())
 
 @app.route('/login', methods=['POST', 'GET'])
 def render_login():
@@ -100,7 +109,7 @@ def render_login():
         session['email'] = email
         session['user_id'] = user_id
         session['firstname'] = first_name
-        session['is_admin'] = is_teacher
+        session['is_teacher'] = is_teacher
         return redirect('/')
 
     return render_template('login.html')
