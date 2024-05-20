4e1bb1aafc318d762050e8dd04bdaff38c9ad68f
Rohit Medarametla <20320@wc.school.nz>
2024-05-20 18:57:53 +1200

    I have made some changes to app.py because i was adjusting spacing to not get pep 8 errors

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-
 # this is a function that creates connection with database and queries what we want and store it in a variable
 def get_list(query, params):
     with create_connection(DATABASE) as con:
@@ -26,6 +25,7 @@ def get_list(query, params):
             cur.execute(query, params)
         query_list = cur.fetchall()
     return query_list
+
 
 def put_data(query, params):
     with create_connection(DATABASE) as con:
@@ -43,26 +43,30 @@ def create_connection(db_file):
         print("Error connecting to database:", e)
         return None
 
+
 def is_logged_in():
     if session.get('email') is None:
         return False
     else:
         return True
 
+
 def is_teacher():
-    if session.get('is_teacher')  != 1:
+    if session.get('is_teacher') != 1:
         print("student")
         return False
     else:
         print("teacher")
         return True
 
+
 def user_detail():
     return session
 
+
 @app.route('/')
 def render_home():  # put application's code here
-    return render_template('home.html', logged_in = is_logged_in())
+    return render_template('home.html', logged_in=is_logged_in())
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -89,12 +93,14 @@ def render_signup():
         hashed_password = bcrypt.generate_password_hash(password)
         try:
             put_data("INSERT INTO user (fname, lname, email, password, is_teacher ) ""VALUES (?, ?, ?, ?, ?)",
-            (fname, lname, email, hashed_password, teacher))
+                     (fname, lname, email, hashed_password, teacher))
+
         except sqlite3.IntegrityError:
             return redirect('\signup?error=Email+is+already+used')
 
         return redirect("\login")
     return render_template('signup.html', logged_in=is_logged_in(), is_teacher=is_teacher())
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def render_login():
@@ -110,8 +116,9 @@ def render_login():
         print(user_data)
         con.close()
 
-        if user_data == None:
-             redirect(request.referrer + '?error=Invalid+username+or+password')
+        if user_data is None:
+            redirect(request.referrer + '?error=Invalid+username+or+password')
+
         else:
             try:
                 user_id = user_data[0]
@@ -120,7 +127,6 @@ def render_login():
                 is_teacher = user_data[3]
             except IndexError:
                 return redirect('/login?error=Invalid+username+or+password')
-
 
             if not bcrypt.check_password_hash(db_password, password):
                 return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
@@ -133,28 +139,31 @@ def render_login():
 
     return render_template('login.html',  logged_in=is_logged_in())
 
-
-
     return render_template("allwords.html", word=words_list,  logged_in=is_logged_in(), categories=category_list)
+
+
 @app.route('/category/<cat_id>')
 def render_category(cat_id):
     title = cat_id
     category_list = get_list("SELECT cat_id, category_name FROM category", "")
     print(category_list)
-    words_list = get_list("SELECT word_id, Maori, English, Definition, level, image, category_name, fname FROM Dictionary m "
+    words_list = get_list("SELECT word_id, Maori, English, Definition, level, image, category_name, fname"
+                          " FROM Dictionary m "
                           "INNER JOIN user u on m.user_id_fk = u.user_id "
                           "INNER JOIN category c ON m.cat_id_fk = c.cat_id WHERE cat_id=?", (cat_id, ))
     print(words_list)
-    return render_template("category.html", word=words_list, categories=category_list, logged_in=is_logged_in(), title=title)
+    return render_template("category.html", word=words_list, categories=category_list,
+                           logged_in=is_logged_in(), title=title)
+
 
 @app.route('/word_detail/<word_id>')
 def render_word_detail(word_id):
     category_list = get_list("SELECT cat_id, category_name FROM category", "")
-    about_word = get_list("SELECT word_id, Maori, English, Definition, level, image, category_name, fname, time_added , entry_date FROM Dictionary m "
+    about_word = get_list("SELECT word_id, Maori, English, Definition, level, image, category_name, fname, "
+                          "time_added , entry_date FROM Dictionary m "
              "INNER JOIN user u on m.user_id_fk = u.user_id "
              "INNER JOIN category c ON m.cat_id_fk = c.cat_id WHERE word_id=?", (word_id,))
     return render_template("word_detail.html", wordinfo=about_word,  logged_in=is_logged_in(), categories=category_list)
-
 
 
 @app.route('/logout')
@@ -163,6 +172,8 @@ def logout():
     [session.pop(key) for key in list(session.keys())]
     print(list(session.keys()))
     return redirect('/?message=See+you+next+time!')
+
+
 @app.route('/admin')
 def render_admin():
     if is_logged_in and is_teacher():
@@ -170,7 +181,9 @@ def render_admin():
         word_detail = get_list("SELECT word_id, English FROM Dictionary", "")
     else:
         return redirect('/?message=Need+to+be+logged+in.')
-    return render_template("admin.html", logged_in=is_logged_in(), is_teacher=is_teacher(), categories=category_list, word_detail=word_detail)
+    return render_template("admin.html", logged_in=is_logged_in(), is_teacher=is_teacher(),
+                           categories=category_list, word_detail=word_detail)
+
 
 @app.route('/add_category', methods=['POST'])
 def add_category():
@@ -180,6 +193,7 @@ def add_category():
         cat_name = request.form.get('name').lower().strip()
         put_data('INSERT INTO category (category_name) VALUES (?)', (cat_name,))
         return redirect('/admin')
+
 
 @app.route('/add_newword', methods=['POST'])
 def add_word():
@@ -197,15 +211,19 @@ def add_word():
         image = "noimage"
         category = category.split(", ")
         cat_id = category[0]
-        put_data('INSERT INTO Dictionary (Maori, English, Definition, level, image,  cat_id_fk, entry_date, user_id_fk, time_added ) VALUES (?,?,?,?,?,?,?,?,?)', (mao_word, eng_word, deff, level, image, cat_id, date_added, user_id, time_added,))
+        put_data('INSERT INTO Dictionary (Maori, English, Definition, level, image,  cat_id_fk, entry_date,'
+                 ' user_id_fk, time_added ) VALUES (?,?,?,?,?,?,?,?,?)',
+                 (mao_word, eng_word, deff, level, image, cat_id, date_added, user_id, time_added,))
 
     return redirect('/admin')
+
 
 @app.route('/edit/<word_id>', methods=['GET', 'POST'])
 def edit_word(word_id):
     if is_logged_in and is_teacher():
         about_word = get_list(
-            "SELECT word_id, Maori, English, Definition, level, image, category_name, fname, entry_date FROM Dictionary m "
+            "SELECT word_id, Maori, English, Definition, level, image, category_name, fname, entry_date"
+            " FROM Dictionary m "
             "INNER JOIN user u on m.user_id_fk = u.user_id "
             "INNER JOIN category c ON m.cat_id_fk = c.cat_id WHERE word_id=?", (word_id,))
         about_word = about_word[0]
@@ -232,7 +250,6 @@ def edit_word(word_id):
     return render_template('edit.html', logged_in=is_logged_in(),  word_de=about_word, word_id=word_id)
 
 
-
 @app.route('/delete_category', methods=['POST'])
 def render_delete_category():
     if not is_logged_in():
@@ -242,8 +259,10 @@ def render_delete_category():
         category = category.split(", ")
         cat_id = category[0]
         cat_name = category[1]
-        return render_template("delete_confirm.html", id=cat_id, category_name=cat_name, type="category", logged_in=is_logged_in(), is_teacher=is_teacher())
+        return render_template("delete_confirm.html", id=cat_id, category_name=cat_name,
+                               type="category", logged_in=is_logged_in(), is_teacher=is_teacher())
     return redirect('/admin')
+
 
 @app.route('/delete_category_confirm/<category_id>')
 def delete_category_confirm(category_id):
@@ -257,6 +276,7 @@ def delete_category_confirm(category_id):
     con.close()
     return redirect('/admin')
 
+
 @app.route('/delete_word/<int:word_id>')
 def render_delete_word(word_id):
     if not is_logged_in():
@@ -265,7 +285,8 @@ def render_delete_word(word_id):
         return redirect('/')
     word_info = get_list("SELECT Maori FROM Dictionary WHERE word_id = ?", (word_id, ))
 
-    return render_template("delete_confirm1.html", id=word_id, name=word_info[0][0], type="word", logged_in=is_logged_in(), is_teacher=is_teacher())
+    return render_template("delete_confirm1.html", id=word_id, name=word_info[0][0], type="word",
+                           logged_in=is_logged_in(), is_teacher=is_teacher())
 
 
 @app.route('/delete_word_confirm/<int:word_id>', methods=['POST'])
@@ -280,13 +301,16 @@ def delete_word_confirm(word_id):
     con.close()
     return redirect('/allwords')
 
+
 @app.route('/search', methods=['GET', 'POST'])
 def render_search():
     search = request.form['search']
     title = "Search results for: " + search
-    query = ("SELECT word_id, Maori, English, definition, level, category_name  FROM Dictionary m INNER JOIN category c ON m.cat_id_fk = c.cat_id "
+    query = ("SELECT word_id, Maori, English, definition, level, category_name  FROM Dictionary m "
+             "INNER JOIN category c ON m.cat_id_fk = c.cat_id "
              "WHERE " 
-            "word_id like ? or Maori like ? OR English like ? OR definition like ? OR level like ? OR category_name like ? ")
+            "word_id like ? or Maori like ? OR English like ? OR definition like ? OR level like ? "
+             "OR category_name like ? ")
     search = "%" + search + "%"
     con = create_connection(DATABASE)
     cur = con.cursor()
@@ -295,14 +319,16 @@ def render_search():
     con.close()
     return render_template("allwords.html", word=search_list, title=title, logged_in=is_logged_in())
 
+
 @app.route('/allwords')
 def table():
-    category_list = get_list("SELECT cat_id, category_name FROM category","")
+    category_list = get_list("SELECT cat_id, category_name FROM category", "")
     words_list = get_list("SELECT word_id, Maori, English, Definition, level, category_name, image  FROM Dictionary m "
              "INNER JOIN category c ON m.cat_id_fk = c.cat_id", "")
     print(words_list)
 
     return render_template("allwords.html", word=words_list, logged_in=is_logged_in(), categories=category_list)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -325,7 +351,6 @@ def add_image_to_database(filename):
     conn.commit()
     conn.close()
     return render_template('upload.html')
-
 
 
 if __name__ == '__main__':
